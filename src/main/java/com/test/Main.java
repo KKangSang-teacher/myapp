6ae175e72594cc484a16162c05c593c35a50a90d
Kang SangHee <todak7712@gmail.com>
2024-07-29 14:21:10 +0900

    ok

---
+++ src/main/java/com/test/Main.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+   
+        System.out.println("Have a good day");
+        Scanner scanner=new Scanner(System.in);
+        String kk=scanner.nextLine();
+        System.out.println(kk);
+
+    }
+}
