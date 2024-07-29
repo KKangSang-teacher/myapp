022818d3f13542fdf0047f407b12beaa975c86a6
Kang SangHee <todak7712@gmail.com>
2024-07-29 17:52:41 +0900

    beans

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -1,15 +1,38 @@
 package com.test;
 
-import java.util.Scanner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-   
-        System.out.println("Have a good day");
-        Scanner scanner=new Scanner(System.in);
-        String kk=scanner.nextLine();
-        System.out.println(kk);
+        // Student student = new Student();
+        // student.setStudentId("1");
+        // student.setStudentName("강상희");
+        // System.out.println(student.getStudentId());
+        // System.out.println(student.getStudentName());
+        
+        // ApplicationContext context= new ClassPathXmlApplicationContext("Beans.xml");
+        // Student student = context.getBean("student", Student.class);
+        // student.setStudentId("2");
+        // student.setStudentName("강감찬");
+        // System.out.println(student.getStudentId());
+        // System.out.println(student.getStudentName());
+
+        // Address address = new Address();
+        // address.setLocation("Seoul");
+        // Employee employee=new Employee();
+        // employee.setName("세종대왕");
+        // employee.setAddress(address);
+        // employee.displayInfo();
+
+        
+        ApplicationContext context= new ClassPathXmlApplicationContext("Beans.xml");
+        Employee employee= context.getBean("employee", Employee.class);
+        employee.setName("세종대왕");
+        Address address= context.getBean("address", Address.class);
+        address.setLocation("Seoul");
+        //employee.setAddress(address);
+        employee.displayInfo();
 
     }
 }
